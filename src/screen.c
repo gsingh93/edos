e0b44a41ba929d328701c0a69b5658699e75d4fc
Gulshan Singh <gulshan@umich.edu>
2013-10-29 23:12:48 -0400

    Added screen scrolling

--- src/screen.c
+++ src/screen.c
@@ -1,6 +1,8 @@
 #include "screen.h"
 #include "portio.h"
 
+static uint16_t *const video_memory = (uint16_t *) 0xB8000;
+
 static const uint8_t ROWS = 25;
 static const uint8_t COLS = 80;
 static uint8_t  cursor_x = 0;
@@ -9,15 +11,24 @@ static uint8_t cursor_y = 0;
 static color_t foreground_color = WHITE;
 static color_t background_color = BLACK;
 
-static void set_video_mem(uint8_t row, uint8_t col, char c);
+static void set_video_mem_char(uint8_t row, uint8_t col, char c);
+static void set_video_mem(uint8_t row, uint8_t col, uint16_t data);
 static void move_cursor(void);
 static void draw_cursor(void);
+static void scroll(void);
 
-static void set_video_mem(uint8_t row, uint8_t col, char c) {
-    static uint16_t *video_memory = (uint16_t *) 0xB8000;
+static void set_video_mem_char(uint8_t row, uint8_t col, char c) {
     uint8_t color = background_color << 4 | foreground_color;
     uint16_t entry = color << 8 | c;
     video_memory[row * COLS + col] = entry;
+}
+
+static void set_video_mem(uint8_t row, uint8_t col, uint16_t data) {
+    video_memory[row * COLS + col] = data;
+}
+
+static uint16_t get_video_mem(uint8_t row, uint8_t col) {
+    return video_memory[row * COLS + col];
 }
 
 static void move_cursor(void) {
@@ -28,10 +39,7 @@ static void move_cursor(void) {
     }
     if (cursor_y == ROWS) {
         cursor_y--;
-        int i;
-        for (i = 0; i < COLS; i++) {
-            set_video_mem(cursor_y, i, ' ');
-        }
+        scroll();
     }
 }
 
@@ -43,11 +51,26 @@ static void draw_cursor(void) {
     outb(0x3D5, pos >> 8);
 }
 
+static void scroll(void) {
+    // Shift all rows up by one
+    int i, j;
+    for (i = 1; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            set_video_mem(i - 1, j, get_video_mem(i, j));
+        }
+    }
+
+    // Clear the last row
+    for (i = 0; i < COLS; i++) {
+        set_video_mem_char(ROWS - 1, i, ' ');
+    }
+}
+
 void cls(void) {
     int i, j;
     for (i = 0; i < COLS; i++) {
         for (j = 0; j < ROWS; j++) {
-            set_video_mem(j, i, ' ');
+            set_video_mem_char(j, i, ' ');
         }
     }
     cursor_x = 0;
@@ -60,12 +83,16 @@ void putchar(const char c) {
     case '\n':
         cursor_x = 0;
         cursor_y++;
+        if (cursor_y >= ROWS) {
+            cursor_y--;
+            scroll();
+        }
         break;
     case '\r':
         cursor_x = 0;
         break;
     default:
-        set_video_mem(cursor_y, cursor_x, c);
+        set_video_mem_char(cursor_y, cursor_x, c);
         move_cursor();
     }
     draw_cursor();
