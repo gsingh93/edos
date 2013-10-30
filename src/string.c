ca91929910588a8da026a75b1fdb83a0a2d3b32a
Gulshan Singh <gulshan@umich.edu>
2013-10-29 23:33:46 -0400

    Implemented memset

--- src/string.c
+++ src/string.c
@@ -1,4 +1,8 @@
 #include "string.h"
 
-void memset(void *obj, uint32_t val, uint32_t size) {
+void memset(void *obj, uint8_t val, uint32_t size) {
+    uint8_t *mem = (uint8_t *) obj;
+    for (int i = 0; i < size; i++) {
+        mem[i] = val;
+    }
 }
